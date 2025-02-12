3e462643a59b3468518d86fbe933731d23ef11fa
Vlad-2109 <vladswim21@gmail.com>
2025-02-12 21:17:47 +0200

    add db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { app } from './app';
+import { error } from 'console';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const mongo_uri = process.env.DB || '';
+
+mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log('DB Connection successful');
+  })
+  .catch((error) => console.log(error));
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
