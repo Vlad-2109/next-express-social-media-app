178a8cdffba4eb34ffeb5953a77ded89d6c00d92
Vlad-2109 <vladswim21@gmail.com>
2025-02-14 21:25:27 +0200

    add sendMail function

---
+++ server/src/utils/email.ts
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+type EmailOptions = {
+  email: string;
+  subject: string;
+  html: string;
+};
+
+export const sendEmail = async (options: EmailOptions) => {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    form: `"PhotoFlow" Share Your Imagination`,
+    to: options.email,
+    subject: options.subject,
+    html: options.html,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
