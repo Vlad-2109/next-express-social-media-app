b2c26d548c6e235472f1e0eaeee5869fc2e73496
Vlad-2109 <vladswim21@gmail.com>
2025-02-15 19:11:01 +0200

    add verify router

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { signup } from '../controllers/authController';
+import { signup, verifyAccount } from '../controllers/authController';
+import isAuthenticated from '../middleware/isAuthenticated';
 const router = Router();
 
 router.post('/signup', signup);
+router.post('/verify', isAuthenticated, verifyAccount);
 
 export default router;
