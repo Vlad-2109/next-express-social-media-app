0c1adfed003d39d7c2ec1513098c16c3e14b57c3
Vlad-2109 <vladswim21@gmail.com>
2025-02-26 21:59:55 +0200

    add leftSidebar

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,7 +1,43 @@
 'use client';
 
+import { MenuIcon } from 'lucide-react';
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetTitle,
+	SheetTrigger,
+} from '../ui/sheet';
+import Feed from './Feed';
+import LeftSidebar from './LeftSidebar';
+import RightSidebar from './RightSidebar';
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className="flex">
+			<div className="w-[20%] hidden md:block border-r-2 h-screen fixed">
+				<LeftSidebar />
+			</div>
+			<div className="flex-1 md:ml-[20%] overflow-y-auto">
+				<div className="md:hidden">
+					<Sheet>
+						<SheetTrigger>
+							<MenuIcon />
+						</SheetTrigger>
+						<SheetContent>
+							<SheetTitle></SheetTitle>
+							<SheetDescription></SheetDescription>
+							<LeftSidebar />
+						</SheetContent>
+					</Sheet>
+				</div>
+				<Feed />
+			</div>
+			<div className="w-[30%] pt-8 px-6 lg:block hidden">
+				<RightSidebar />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
