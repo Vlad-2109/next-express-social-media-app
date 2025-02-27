5c956698db6f7e4a79d09d37220c483dfa108168
Vlad-2109 <vladswim21@gmail.com>
2025-02-27 19:18:26 +0200

    add rightSidebar section

--- client/src/components/Home/RightSidebar.tsx
+++ client/src/components/Home/RightSidebar.tsx
@@ -7,6 +7,8 @@ import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import { BASE_API_URL } from '../../../server';
 import { handleRequest } from '../utils/apiRequest';
+import { Loader } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
 const RightSidebar = () => {
 	const user = useAppSelector((state) => state.auth.user);
@@ -30,7 +32,65 @@ const RightSidebar = () => {
 		};
 		getSuggestedUser();
 	}, []);
-	return <div>RightSidebar</div>;
+
+	if (isLoading) {
+		return (
+			<div className="w-full h-screen flex flex-col items-center justify-center">
+				<Loader className="animate-spin" />
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<div className="flex items-center justify-between">
+				<div className="flex items-center space-x-4">
+					<Avatar className="w-9 h-9">
+						<AvatarImage
+							src={user?.profilePicture}
+							className="h-full w-full rounded-full"
+						/>
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>
+					<div>
+						<h1 className="font-bold">{user?.username}</h1>
+						<p className="text-gray-700">
+							{user?.bio || 'My Profile Bio Here'}
+						</p>
+					</div>
+				</div>
+				<h1 className="font-medium text-blue-700 cursor-pointer">Switch</h1>
+			</div>
+			<div className="flex items-center justify-between mt-8">
+				<h1 className="font-semibold text-gray-700">Suggested User</h1>
+				<h1 className="font-medium cursor-pointer">See all</h1>
+			</div>
+			{suggestedUser?.slice(0, 5).map((s_user) => (
+				<div key={s_user._id} className="mt-6 cursor-pointer">
+					<div className="flex items-center justify-between">
+						<div className="flex items-center space-x-4 cursor-pointer">
+							<Avatar className="w-9 h-9">
+								<AvatarImage
+									src={s_user.profilePicture}
+									className="h-full w-full rounded-full"
+								/>
+								<AvatarFallback>CN</AvatarFallback>
+							</Avatar>
+							<div>
+								<h1 className="font-bold">{s_user.username}</h1>
+								<p className="text-gray-700">
+									{s_user.bio || 'My Profile Bio Here'}
+								</p>
+							</div>
+						</div>
+						<h1 className="font-medium text-blue-700 cursor-pointer">
+							Details
+						</h1>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default RightSidebar;
