5280ef583325c06ea2d40c771ccb5283b172cb64
Vlad-2109 <vladswim21@gmail.com>
2025-03-15 21:51:36 +0200

    fix params in profile page

--- client/src/app/profile/[id]/page.tsx
+++ client/src/app/profile/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Profile from '@/components/Profile/Profile';
 
-const ProfilePage = async ({ params }: { params: { id: string } }) => {
+const ProfilePage = async ({ params }: { params: Promise<{ id: string }> }) => {
 	const id = (await params).id;
 
 	return <Profile id={id} />;
